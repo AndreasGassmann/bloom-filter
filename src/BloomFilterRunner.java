e71aa7fbd21403cd814935e977c4a142fea7654a
Jonas Frehner <jonas@frehner.ch>
2015-11-30 15:15:19 +0100

    - Updated: Document BloomFilterRunner

--- src/BloomFilterRunner.java
+++ src/BloomFilterRunner.java
@@ -6,17 +6,30 @@ import java.nio.file.Paths;
 public class BloomFilterRunner {
     public static void main(String[] args) throws IOException {
 
-
+    	//Set the path to the initial text-file.
         Path path = Paths.get("assets/", "words.txt");
+
+        //Set the path to the "test-text-file".
         Path pathDeutsch = Paths.get("assets/", "deutsch.txt");
 
-        BloomFilter bf = new BloomFilter(58111, 0.5);
+        //Construct a new Bloom-Filter with 58111-elements and an
+        //error-probability of 5%.
+        BloomFilter bf = new BloomFilter(58111, 0.05);
 
+        //Hash every line of the initial file and add the word to the data-
+        //structure of the bloom-filter.
         Files.lines(path).forEach(s -> bf.addToFilter(s));
 
         int counter = 0;
         int linesDeutsch = 71700;
 
+        /**
+         * TEST-SECTION
+         * Tests the bloom-filter.
+         */
+
+        //Calculate for every line (aka word) of the test-file if
+        //it is already a member of the data-structure.
         Object[] strings = Files.lines(pathDeutsch).toArray();
         for(Object s : strings) {
             if (bf.checkIfExists((String) s)) {
@@ -33,20 +46,5 @@ public class BloomFilterRunner {
 
         System.out.println(counter + "/" + bf.booleans.length);
 
-        /*
-        int counter = 0;
-        int counterInner = 0;
-
-        Object[] strings = Files.lines(pathDeutsch).toArray();
-        Object[] strings1 = Files.lines(path).toArray();
-
-
-            for(Object d : strings1) {
-                if (s.equals(d)) {
-                    System.out.println(s);
-                }
-            }
-        }
-        */
     }
 }
